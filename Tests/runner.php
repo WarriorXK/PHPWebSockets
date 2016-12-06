f6b17d9967f6a8416c80690e8eed12f317e02e95
kevinmeijer <kevin.meijer@thefrontdoor.nl>
2016-12-06 14:59:12 +0100

    Implemented test runner

--- Tests/runner.php
+++ Tests/runner.php
@@ -1,3 +1,11 @@
 <?php
 
-var_dump($argv);
+$testCase = $argv[1] ?? NULL;
+switch ($testCase) {
+    case 'server':
+    case 'client':
+        require_once __DIR__ . DIRECTORY_SEPARATOR . $testCase . '.php.inc';
+        break;
+    default:
+        throw new \Exception('Unknown test case: ' . $testCase);
+}
