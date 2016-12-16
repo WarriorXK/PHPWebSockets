3abd054dfaa6cce0428b2a5f5de2c283eb8219ad
Kevin Meijer <kevkevm@gmail.com>
2016-12-16 08:14:29 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- Tests/runner.php
+++ Tests/runner.php
@@ -30,7 +30,7 @@ declare(strict_types = 1);
  */
 
 if (php_sapi_name() !== 'cli') {
-    echo('The tests can only be executed in CLI!' . PHP_EOL);
+    echo 'The tests can only be executed in CLI!' . PHP_EOL;
     die(1);
 }
 
